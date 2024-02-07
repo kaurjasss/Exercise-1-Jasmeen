2bff32a86b8cb7247a24a8289b55ce0b559cbfd9
Jasmeen Kaur <kau15652@sheridancollege.ca>
2024-02-06 22:41:08 -0500

    My SECOND COMMIT

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -1,45 +1,29 @@
 package exercise1;
-
-/**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * @author jasmeen kaur 6 february 2024
- */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
+    private String suit;
+    private int value;
+
+    public Card(String suit, int value) {
+        this.suit = suit; 
+        this.value = value;
+    }
+
+    public String getSuit() {  
         return suit;
     }
 
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-
-    /**
-     * @return the value
-     */
     public int getValue() {
         return value;
     }
 
-    /**
-     * @param value the value to set
-     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
     public void setValue(int value) {
         this.value = value;
-    }  
+    }
 }
--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,65 +1,80 @@
 package exercise1;
-
 /**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
- * To be used as starting code in Exercise
- * @author jasmeen kaur february 6 2024 
+ * A class that models playing card Objects. Cards have 
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
+ * A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers.
+ * This code is to be used in ICE1. When you create your own branch,
+ * add your name as a modifier.
+ * @author Jasmeen Kaur February 6, 2024
  */
-public class CardTrick {
-    
-    public static void main(String[] args) {
-        
-        Card[] hand = new Card[7];
+import java.util.Scanner;
 
-        for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+public class CardTrick {
+
+    public static void main(String[] args) {
+        Card[] magicHand = new Card[7];
+
+        // Fill magicHand with random cards
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card(Card.SUITS[(int) (Math.random() * 4)], (int) (Math.random() * 13) + 1);
+            magicHand[i] = c;
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+        // Print the magicHand
+        for (Card card : magicHand) {
+            System.out.println(card.getSuit() + " " + card.getValue());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a card value (1-13): ");
+        int userValue = scanner.nextInt();
+
+        System.out.print("Enter a suit (0-3 where 0-Hearts, 1=Diamonds, 2=Clubs, 3=Spades): ");
+        int userSuitIndex = scanner.nextInt();
+
+        if (userSuitIndex < 0 || userSuitIndex >= Card.SUITS.length) {
+            System.out.println("Invalid suit index entered.");
+            return;
+        }
+
+        String userSuit = Card.SUITS[userSuitIndex];
+
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userValue && card.getSuit().equals(userSuit)) {
+                found = true;
+                break;
+            }
+        }
+
+        // result
+        if (found) {
+            System.out.println("Congratulations, your card is in the magic hand!");
+            printInfo();
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
     }
 
-    /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
-     */
-    private static void printInfo() {
-    
+    public static void printInfo() {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is jasmeen , but you can call me prof, jas or sir");
+        System.out.println("My name is jasmeen kaur, You can call me jas!!");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
+        System.out.println(" I would like to be a software developer");
+        System.out.println(" Travel the world with my family");
+        System.out.println(" helping people");
+        System.out.println();    
 
-        System.out.println("My hobbies: playing basketball");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("My hobbies:");
+        System.out.println(" playing basketball");
+        System.out.println(" Reading");
+        System.out.println(" binge watching netflix");
 
         System.out.println();
-        
-    
     }
-
 }
